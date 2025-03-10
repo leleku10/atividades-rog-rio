3b6d3a33b5d80118853e39f730218b177217aca9
ADILSON LEONARDO CABRAL <adilson.cabral@alunos.sc.senac.br>
2025-03-10 20:15:27 -0300

    Subindo segunda atividade!

--- Projeto01/MainPage.xaml.cs
+++ Projeto01/MainPage.xaml.cs
@@ -2,17 +2,40 @@
 
 namespace MeuProjetoMaui
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-		private async void OnNavigateButtonClick(object sender, EventArgs e)
-		{
-			// Código para navegar para a segunda página
-			await Navigation.PushAsync(new SecondPage());
-		}
-	}
+        // Exibe um alerta simples
+        private async void OnAlertButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alerta", "Este é um alerta simples.", "OK");
+        }
+
+        // Exibe um alerta de confirmação
+        private async void OnConfirmButtonClicked(object sender, EventArgs e)
+        {
+            bool resposta = await DisplayAlert("Confirmar", "Você tem certeza que deseja continuar?", "Sim", "Não");
+
+            if (resposta)
+            {
+                welcomeLabel.Text = "Você clicou em Sim!";
+            }
+            else
+            {
+                welcomeLabel.Text = "Você clicou em Não!";
+            }
+        }
+
+        // Exibe um action sheet para o usuário escolher uma opção
+        private async void OnActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string escolha = await DisplayActionSheet("Escolha uma opção", "Cancelar", null, "Opção 1", "Opção 2", "Opção 3");
+
+            welcomeLabel.Text = $"Você escolheu: {escolha}";
+        }
+    }
 }
